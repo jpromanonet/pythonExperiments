a13bba367355186305e50446251b714df8d392d5
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-07-02 16:34:46 -0300

    ATBSWP | First Commit

---
+++ automatingTheBoringStuffWithPython/cap1/program1.py
@@ -0,0 +1,29 @@
+# This program says hello and ask for my name
+
+# Printing message
+
+print('Hello World')
+print('What is your name?')
+
+# Creating variable and input
+
+myName = input()
+
+# More messages and tools
+
+
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+
+# Asking for age, more inputs
+
+print('What is your age?')
+
+# Creating variable and input
+
+myAge = input()
+
+# More messages
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
